a0cc348f304b1ec36cfcd820e17d04d6d2f786c8
CatsAreMLG <joshuadgon@gmail.com>
2019-09-24 19:44:12 -0500

    Add Item tableto admin page

--- commands/admin.py
+++ commands/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Room, Player
+from .models import Room, Player, Item
 
 admin.site.site_header = "MUD Admin"
 admin.site.site_title = "MUD Admin Area"
@@ -7,3 +7,4 @@ admin.site.site_header = "Welcome to MUD Admin"
 
 admin.site.register(Room)
 admin.site.register(Player)
+admin.site.register(Item)
